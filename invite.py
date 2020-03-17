f2435089d5ffc11bbe21ead324deb7ee067235fd
szvds <62033452+szvds@users.noreply.github.com>
2020-03-17 13:03:04 -0100

    Update invite.py

--- invite.py
+++ invite.py
@@ -12,7 +12,7 @@ async def on_ready():
 	global role_list
 	print('Logged in as: '+client.user.name)
 	print('Bot ID: '+client.user.id)
-	await client.change_presence(game=discord.Game(name='TZ!invites - List your invites')) '''Watching invites '''
+	await client.change_presence(game=discord.Game(name='/invites')) 
 	print('------\n')
 	for server in client.servers:
 		role_list=dict((role.name,role) for role in server.roles)
@@ -37,7 +37,7 @@ async def on_member_join(new_member):
 
 @client.event
 async def on_message(message):
-	if message.content=='TZ!invites': '''Here is the command section'''
+	if message.content=='/invites': 
 		total_uses=0
 		embed=discord.Embed(title='__Invites from {}__'.format(message.author.name))
 		invites = await client.invites_from(message.server)
@@ -51,4 +51,4 @@ async def on_message(message):
 		await client.send_message(message.channel,embed=embed)
 
 
-client.run('Njg5NDYwMjMxMjUxNDI3Mzg3.XnDNpw.2WQusVYJuk28_3HVxFSZWb2eXsg') '''Copy My Token'''
+client.run('Njg5NDYwMjMxMjUxNDI3Mzg3.XnDNpw.2WQusVYJuk28_3HVxFSZWb2eXsg')
